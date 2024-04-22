67cd3c9c3772ce042e7fd4cb8998a60b9b9a0e6e
Julius <juliushubilla@gmail.com>
2024-04-22 17:19:22 +0800

    Integrated Backend admin dashboard, admin page, and auth page -- 4th commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,15 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('admin', static function($routes){
+
+  $routes->group('',[], static function($routes){
+    $routes->view('example-page','example-page');
+  });
+
+  $routes->group('',[], static function($routes){
+    $routes->view('example-auth','example-auth');
+  });
+
+});
---
+++ app/Views/example-auth.php
@@ -0,0 +1,4 @@
+<?= $this->extend('backend/layout/auth-layout') ?>
+<?= $this->section('content') ?>
+--- Auth Page here
+<?= $this->endSection() ?>
