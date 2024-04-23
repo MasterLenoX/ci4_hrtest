d16d6749c2880073293ef7b20c65be47163025ac
Julius <juliushubilla@gmail.com>
2024-04-23 14:24:31 +0800

    Protecting Routes & Filters --- 6th commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,13 +9,13 @@ $routes->get('/', 'Home::index');
 
 $routes->group('admin', static function($routes){
 
-  $routes->group('',[], static function($routes){
+  $routes->group('',['filter'=>'cifilter:auth'], static function($routes){
     // $routes->view('example-page','example-page');
     $routes->get('home','AdminController::index',['as'=>'admin.home']);
     $routes->get('logout','AdminController::logoutHandler',['as'=>'admin.logout']);
   });
 
-  $routes->group('',[], static function($routes){
+  $routes->group('',['filter'=>'cifilter:guest'], static function($routes){
     // $routes->view('example-auth','example-auth');
     $routes->get('login','AuthController::loginForm',['as'=> 'admin.login.form']);
     $routes->post('login','AuthController::loginHandler',['as'=>'admin.login.handler']);
